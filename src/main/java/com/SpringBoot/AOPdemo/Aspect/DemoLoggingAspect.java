eb59e3905a12868ccea1be4d64d7752032d57c2b
Ojus M S <ojusms@gmail.com>
2026-08-03 19:13:10 -0700

    Add Aspect package, DemoLoggingAspect class with an @Before advice method.

---
+++ src/main/java/com/SpringBoot/AOPdemo/Aspect/DemoLoggingAspect.java
@@ -0,0 +1,21 @@
+package com.SpringBoot.AOPdemo.Aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+// annotated with @Component to enable detection for component scanning
+// annotated with @Aspect to let Spring know this class is an Aspect
+@Component
+@Aspect
+public class DemoLoggingAspect {
+
+    // all related advices for logging are added here. Starting with an @Before advice.
+
+    // annotated with @Before since this advice executes before execution of target object addAccount() in the
+    // pointcut expression. The method name can be anything of choice.
+    @Before("execution(public void addAccount())")
+    public void beforeAddAccountAdvice() {
+        System.out.println("==========> Executing @Before advice on addAccount() <==========");
+    }
+}
