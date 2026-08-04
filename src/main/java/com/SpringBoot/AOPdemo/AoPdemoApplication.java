26fe9d507a8e670d360a961164c8e2a90e007d9b
Ojus M S <ojusms@gmail.com>
2026-08-03 17:42:39 -0700

    Initial commit - Spring Boot project from Spring Initializr

---
+++ src/main/java/com/SpringBoot/AOPdemo/AoPdemoApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.AOPdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoPdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AoPdemoApplication.class, args);
+	}
+
+}
