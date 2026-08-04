c7b1b0472932f8aeb16c26e21432bf1244af4bc4
Ojus M S <ojusms@gmail.com>
2026-08-03 18:07:30 -0700

    Add Maven dependency for Spring Boot Starter AspectJ in pom.xml, add a CommandLineRunner method in the main app to print a msg on run

--- src/main/java/com/SpringBoot/AOPdemo/AoPdemoApplication.java
+++ src/main/java/com/SpringBoot/AOPdemo/AoPdemoApplication.java
@@ -1,13 +1,22 @@
 package com.SpringBoot.AOPdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AoPdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AoPdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World!");
+		};
 	}
 
 }
