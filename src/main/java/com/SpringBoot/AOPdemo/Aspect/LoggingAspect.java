e063b73173bae57bc31366241e77008894397d61
Ojus M S <ojusms@gmail.com>
2026-08-05 19:07:30 -0700

    Update to capture and display Method Signature using JoinPoint in Logging Aspect

--- src/main/java/com/SpringBoot/AOPdemo/Aspect/LoggingAspect.java
+++ src/main/java/com/SpringBoot/AOPdemo/Aspect/LoggingAspect.java
@@ -1,9 +1,11 @@
 package com.SpringBoot.AOPdemo.Aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;;
+import org.springframework.stereotype.Component;
 
 // annotated with @Component to enable detection for component scanning
 // annotated with @Aspect to let Spring know this class is an Aspect
@@ -17,8 +19,12 @@ public class LoggingAspect {
     // all related advices for logging are added here. Starting with an @Before advice.
     // have to use fully qualified name since Pointcut Declarative exists in a separate file now
     @Before("com.SpringBoot.AOPdemo.Utility.aopExpressions.forDAOnoGetterSetter()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint joinPoint) {
         System.out.println("\n==========> Executing @Before advice on any method() <==========");
+        // JoinPoint contains metadata about method call
+        // Display method signature using JoinPoint
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature(); // casting Signature to MethodSig..
+        System.out.println("Signature: "+methodSignature);
     }
 }
 
