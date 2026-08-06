06aa89e0df78d256ce68987c6926d098a8044b1a
Ojus M S <ojusms@gmail.com>
2026-08-05 18:17:19 -0700

    Updates to demonstrate ordering of applying Aspects by using Order annotation

--- src/main/java/com/SpringBoot/AOPdemo/Aspect/CloudLogAspect.java
+++ src/main/java/com/SpringBoot/AOPdemo/Aspect/CloudLogAspect.java
@@ -2,10 +2,12 @@ package com.SpringBoot.AOPdemo.Aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class CloudLogAspect {
     @Before("com.SpringBoot.AOPdemo.Utility.aopExpressions.forDAOnoGetterSetter()")
     public void performCloudLog() {
--- src/main/java/com/SpringBoot/AOPdemo/Aspect/LoggingAspect.java
+++ src/main/java/com/SpringBoot/AOPdemo/Aspect/LoggingAspect.java
@@ -2,12 +2,16 @@ package com.SpringBoot.AOPdemo.Aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;;
 
 // annotated with @Component to enable detection for component scanning
 // annotated with @Aspect to let Spring know this class is an Aspect
+// annotated with @Order to indicate the order of applying Aspects to Spring. Otherwise, no fixed order.
+// Lower number has higher precedence. Numbers do not have to be contiguous. -ve numbers can also be used.
 @Component
 @Aspect
+@Order(1)
 public class LoggingAspect {
 
     // all related advices for logging are added here. Starting with an @Before advice.
